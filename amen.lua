0e74b7ce9d350dff9c06baad406a601295fba7d7
Zack Scholl <zack.scholl@gmail.com>
2021-04-02 19:37:50 -0700

    loop maker works

--- amen.lua
+++ amen.lua
@@ -2,16 +2,16 @@
 -- get that amen break.
 --
 
-local UI=require "ui"
 -- amenbreaks=include("amen/lib/amen")
 
-engine.name="Amen"
-
-beat_num=16
-primed=false
+shift=false
+beat_num=2
 recording=false
 playing=false
 current_pos={0,0}
+last_pos=0
+loop_points={0,0}
+window={0,0}
 -- WAVEFORMS
 waveform_samples={{}}
 
@@ -19,195 +19,215 @@ function init()
   -- amen=amenbreaks:new()
 
   -- initiate softcut
+  audio.level_adc_cut(1)
+  audio.level_eng_cut(0)
+  audio.level_tape_cut(1)
   softcut.reset()
   for i=1,2 do
-    softcut.level(i,1)
-    softcut.rec_level(i,1)
-    softcut.pre_level(i,0)
-    softcut.buffer(i,i)
-    softcut.pan(i,util.linlin(1,2,-1,1,i))
+    softcut.enable(i,1)
+    softcut.pan(i,i*2-3)
+    softcut.level_input_cut(i,i,1)
+    softcut.play(i,0)
+    softcut.rate(i,1)
+    softcut.loop_start(i,0)
+    softcut.loop_end(i,clock.get_beat_sec()*beat_num*2)
     softcut.loop(i,1)
     softcut.fade_time(i,0)
-    softcut.loop_start(i,0)
-    softcut.loop_end(i,clock.get_beat_sec()*beat_num) -- TODO: calculate based on bpm
+    softcut.rec_level(i,1)
+    softcut.pre_level(i,0)
     softcut.rec(i,0)
-    softcut.enable(i,1)
+    softcut.position(i,0)
+    softcut.buffer(i,i)
+    softcut.level(i,1)
 
     softcut.level_slew_time(i,0)
+    softcut.pan_slew_time(i,0)
     softcut.rate_slew_time(i,0)
     softcut.recpre_slew_time(i,0)
-    softcut.level_eng_cut(1)
-    softcut.level_adc_cut(0)
-    softcut.level_tape_cut(0)
     softcut.phase_quant(i,0.025)
 
-    softcut.post_filter_dry(i,0.0)
-    softcut.post_filter_lp(i,1.0)
-    softcut.post_filter_rq(i,1.0)
-    softcut.post_filter_fc(i,20100)
-
-    softcut.pre_filter_dry(i,1.0)
-    softcut.pre_filter_lp(i,1.0)
-    softcut.pre_filter_rq(i,1.0)
-    softcut.pre_filter_fc(i,20100)
+    softcut.render_buffer(i,0,clock.get_beat_sec()*beat_num*2,128)
   end
-  for i=3,6 do
-    softcut.enable(i,0)
-  end
-  softcut.render_buffer(1,0,clock.get_beat_sec()*beat_num,128)
-  softcut.render_buffer(2,0,clock.get_beat_sec()*beat_num,128)
-  softcut.event_render(on_render)
+  softcut.event_render(function(ch,start,i,s)
+    waveform_samples[ch]=s
+  end)
   softcut.event_phase(function(i,x)
+    -- print(i,x)
     current_pos[i]=x
   end)
   softcut.poll_start_phase()
-
-  -- initate recording on incoming audio
-  polls={"amp_in_l","amp_in_r"}
-  polling={}
-  for i,p in ipairs(polls) do
-    polling[i]=poll.set(p)
-    polling[i].time=1
-    polling[i].callback=function(val)
-      if val>8/10000 and primed then
-        recording_start()
-      end
-    end
-    polling[i].start()
-  end
-
-  clock.run(redraw_clock)
+  loop_points={0,clock.get_beat_sec()*beat_num}
+  window={0,clock.get_beat_sec()*beat_num*2}
+  runner=metro.init()
+  runner.time=1/30
+  runner.count=-1
+  runner.event=runner_f
+  runner:start()
 end
 
 function recording_start()
-  if not primed or recording then
+  if recording then
     do return end
   end
-  recording_unarm()
+  print("recording_start")
   recording=true
   for i=1,2 do
-    softcut.position(i,0)
+    softcut.position(i,window[1])
+    softcut.loop_start(i,window[1])
+    softcut.loop_end(i,window[2]) -- TODO: calculate based on bpm
     softcut.rec_level(i,1)
     softcut.rec(i,1)
-    softcut.play(i,1)
-    softcut.loop(i,0)
+    softcut.play(i,0)
   end
-  engine.recorder_amp(1)
-  audio.level_monitor(0)
+  last_pos=-1
 end
-
-function recording_unarm()
-  primed=false
-  polling[1].time=1
-  polling[2].time=1
-end
-
-function recording_arm()
-  if recording or primed then
-    do return end
-  end
-  primed=true
-  polling[1].time=0.02
-  polling[2].time=0.02
-  engine.recorder_amp(0)
-  audio.level_monitor(1)
-end
-
 
 function recording_stop()
   if not recording then
     do return end
   end
+  print("recording_stop")
   recording=false
-  audio.level_eng_cut(0)
   for i=1,2 do
-    softcut.position(i,0)
+    softcut.position(i,window[1])
     softcut.rec_level(i,0)
     softcut.rec(i,0)
     softcut.play(i,0)
   end
 end
 
-function play()
-  if recording or primed then
+function playback_start()
+  if recording then
     do return end
   end
-  print("play")
+  print("playback_start")
+  playing=true
   for i=1,2 do
-    softcut.position(i,0)
+    softcut.position(i,loop_points[1])
+    softcut.loop_start(i,loop_points[1])
+    softcut.loop_end(i,loop_points[2])
     softcut.play(i,1)
-    softcut.loop(i,1)
   end
 end
 
-function stop()
+function playback_stop()
   if not playing then
     do return end
   end
-  print("stop")
+  print("playback_stop")
+  playing=false
   for i=1,2 do
     softcut.position(i,0)
     softcut.play(i,0)
   end
 end
 
-function on_render(ch,start,i,s)
-  waveform_samples[ch]=s
-end
 
 function enc(k,d)
   if k==1 then
-    beat_num=beat_num+sign(d)
-    for i=1,2 do
-      softcut.loop_end(i,clock.get_beat_sec()*beat_num) -- TODO: calculate based on bpm
-      softcut.render_buffer(i,0,clock.get_beat_sec()*beat_num,128)
+    if not shift then
+      local zoom=0.75
+      if d<0 then
+        zoom=1.5
+      end
+      local di=zoom*math.abs(loop_points[1]-window[1])
+      local di2=zoom*math.abs(loop_points[1]-window[2])
+      if di2>di then
+        di=di2
+      end
+      window[1]=loop_points[1]-di
+      window[1]=util.clamp(window[1],0,120)
+      window[2]=loop_points[1]+di
+      -- else
+      --   window[2]=util.clamp(window[2]+d/10,window[1],120)
     end
+    for i=1,2 do
+      softcut.render_buffer(i,window[1],window[2]-window[1],128)
+    end
+  elseif k==2 then
+    loop_points[1]=util.clamp(loop_points[1]+d/100*(window[2]-window[1]),0,120)
+    loop_points[2]=loop_points[1]+clock.get_beat_sec()*beat_num
+  else
+    beat_num=util.clamp(beat_num+sign(d),1,64)
+    loop_points[2]=loop_points[1]+clock.get_beat_sec()*beat_num
   end
-
 end
 
 function key(k,z)
-  if k==2 and z==1 then
-    if not primed and not recording then
-      recording_arm()
-    elseif primed and not recording then
-      recording_unarm()
+  if k==1 then
+    shift=z==1
+  elseif k==2 and z==1 then
+    if not recording then
+      recording_start()
     elseif recording then
       recording_stop()
     end
   elseif k==3 and z==1 then
+    if recording then
+      recording_stop()
+    end
     if not playing then
-      play()
+      playback_start()
     else
-      stop()
+      playback_stop()
     end
   end
 end
 
-function redraw_clock() -- our grid redraw clock
-  while true do -- while it's running...
-    clock.sleep(1/30) -- refresh
-    -- TODO check if position is too high and then auto turn off recording
-    redraw()
+function runner_f(c) -- our grid redraw clock
+  if recording then
+    for i=1,2 do
+      softcut.render_buffer(i,window[1],window[2]-window[1],128)
+    end
+    if last_pos>current_pos[1] then
+      recording_stop()
+    end
+    last_pos=current_pos[1]
   end
+  redraw()
 end
 
 function redraw()
   screen.clear()
   screen.level(15)
-  screen.move(8,8)
-  screen.text("loop recorder, beats="..beat_num)
-  screen.level(15)
-  waveform_height=20
-  waveform_center=58
+  screen.move(2,8)
+  if playing then
+    screen.text(beat_num.." beat loop [playing]")
+  elseif recording then
+    screen.text(beat_num.." beat loop [recording]")
+  else
+    screen.text(beat_num.." beat loop ")
+  end
+
+  waveform_height=80
+  waveform_center=38
+  local lp={}
+  lp[1]=util.round(util.linlin(window[1],window[2],1,128,loop_points[1]))
+  lp[2]=util.round(util.linlin(window[1],window[2],1,128,loop_points[2]))
+  local pos=util.round(util.linlin(window[1],window[2],1,128,current_pos[1]))
   if waveform_samples[1]~=nil and waveform_samples[2]~=nil then
     for j=1,2 do
       for i,s in ipairs(waveform_samples[j]) do
         local height=util.clamp(0,waveform_height,util.round(math.abs(s)*waveform_height))
+        screen.level(10)
+        if i<lp[1] or i>lp[2] then
+          screen.level(5)
+        end
+        if pos==i then
+          screen.level(15)
+        end
         screen.move(i,waveform_center)
         screen.line_rel(0,(j*2-3)*height/2)
         screen.stroke()
       end
+    end
+  end
+  for i=1,2 do
+    if lp[i]~=128 then
+      screen.level(15)
+      screen.move(lp[i],10)
+      screen.line_rel(0,80)
+      screen.stroke()
     end
   end
   screen.update()
